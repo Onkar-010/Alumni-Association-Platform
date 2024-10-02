5b7ad3f61052ca55f87b16d67eb1b45964a158d2
Onkar-010 <onkarpawar7924@gmail.com>
2024-10-02 15:51:16 +0530

    implement RightSidebar UI Only

--- Sources/js/login.js
+++ Sources/js/login.js
@@ -1,19 +1,25 @@
+const content = document.querySelector(".container");
+const registerForm = document.getElementById("registerForm");
+
+// First form submission (registration form)
+// Prevent form submission
+
 // Function to replace the entire HTML content with the verification page
 function showVerificationPage() {
   // Replace the body content with the verification page's HTML
-  document.body.innerHTML = `
-        <div class="verifiedPage">
-          <div
-            id="lottie-animation2"
-            style="
-              width: 40rem;
-              height: 40rem;
-              align-items: center;
-              background: #2c73eb;
-            "
-          ></div>
-        </div>
-      `;
+  document.content.innerHTML = `
+  <div class="verifiedPage">
+    <div
+      id="lottie-animation2"
+      style="
+        width: 40rem;
+        height: 40rem;
+        align-items: center;
+        background: #2c73eb;
+      "
+    ></div>
+  </div>
+`;
 
   // Reinitialize the Lottie animation after the HTML content is replaced
   var animation2 = lottie.loadAnimation({
@@ -23,6 +29,12 @@ function showVerificationPage() {
     autoplay: true,
     path: "Sources/images/Animation - 1727103464218.json",
   });
+
+  // Simulate a verification process by using a timer
+  setTimeout(function () {
+    // Redirect to the dashboard page after verification (you can customize this)
+    window.location.href = "Dashboard.html";
+  }, 1500); // 5-second delay before redirecting
 }
 
 // Add event listener to the login button
